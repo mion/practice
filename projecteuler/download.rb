f50d7c53bc847c1c1027f37d44bf7b5c93966e54
Gabriel Vieira <gluisvieira@gmail.com>
2016-08-12 02:35:07 -0300

    update downloader WIP

--- projecteuler/download.rb
+++ projecteuler/download.rb
@@ -1,28 +1,41 @@
 require "harvestman"
+require "pry"
 
 problem_id = 2
 
-class Problem
+class ProblemDownloader
   attr_accessor :title, :wording
 
-  def initialize(**opts)
-    @problem_id = opts[:id]
+  def initialize(id)
+    @problem_id = id
   end
 
   def download
-    Harvestman.crawl("https://projecteuler.net/problem=#{problem_id}") do
-      @title = css("#content > h2").inner_text
-      @wording = css(".problem_content").inner_text
+    puts "Downloading problem with ID = '#{@problem_id}'"
+    result = {}
+    Harvestman.crawl("https://projecteuler.net/problem=#{@problem_id}") do
+      result[:title] = css("#content > h2").inner_text.strip
+      result[:wording] = css(".problem_content").inner_text.strip
     end
+    result
   end
 end
 
 def main
-  "[ERROR] You need to specify a problem ID" if ARGV.empty?
+  return "[ERROR] You need to specify a problem ID" if ARGV.empty?
 
   id = ARGV[0]
-  problem = Problem.new(id: id)
-  # parei aqui
+  problem_downloader = ProblemDownloader.new(id)
+  problem = problem_downloader.download
+  title = problem[:title]
+  lines = problem[:wording].split("\n")
+  puts ";" * 80
+  puts ";; #{title}"
+  lines.each do |line|
+    puts ";;\t #{line}"
+  end
+  puts ";" * 80
+  puts "\n#lang racket\n"
 end
 
 puts main()
