2fa29e6bf22e9a4a81dd32577dafa0ef49d33968
Gabriel Vieira <gluisvieira@gmail.com>
2016-08-13 14:26:25 -0300

    Implement simple prime algo

--- projecteuler/p3.rkt
+++ projecteuler/p3.rkt
@@ -3,3 +3,23 @@
 ;; What is the largest prime factor of the number 600851475143 ?
 #lang racket
 
+(define-syntax-rule (while condition body ...)
+  (let loop ()
+    (when condition
+      body ...
+      (loop))))
+
+(define (any fn lst)
+    (not (empty? (filter fn lst))))
+
+(define (odd-numbers-up-to n)
+  (filter odd? (build-list (inexact->exact (round (add1 n))) values)))
+
+(define (prime? n)
+  (cond [(<= n 1) #f]
+        [(= n 2) #t]
+        [(even? n) #f]
+        [else (empty? (filter (lambda (m) (= 0 (remainder n m))) (cdr(odd-numbers-up-to (sqrt n)))))]))
+
+
+               
